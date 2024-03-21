bdaa3f7966b74a9ebe454fd0867939f74375214c
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-03-21 16:21:37 +0530

    add route and views folder

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const articleRouter = require("./routes/articles");
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.send("I am working root");
-})
+  const articles = [
+    {
+      title: "Test Article1",
+      createdAt: new Date(),
+      description: "Test description",
+    },
+    {
+      title: "Test Article2",
+      createdAt: new Date(),
+      description: "Test description",
+    },
+  ];
+  res.render("index", { articles: articles });
+});
+
+app.use("/articles", articleRouter);
 
 app.listen(3000, () => {
-    console.log(`Server is listening to port 3000`);
+  console.log(`Server is listening to port 3000`);
 });
