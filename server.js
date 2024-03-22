6b3222f505bb8a56241b9903e9d7298ed6c6af13
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-03-23 00:48:39 +0530

    add features

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotevn = require("dotenv");
+const bodyParser = require("body-parser");
 const articleRouter = require("./routes/articles");
+const Article = require("./models/articles");
+const methodOverride = require("method-override");
 
+
+dotevn.config();
+app.use(express.urlencoded({extended: true}));      // To encode the data that comes from the form....
+app.use(methodOverride('_method'));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test Article1",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-    {
-      title: "Test Article2",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-  ];
-  res.render("index", { articles: articles });
+
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(`mongodb+srv://${username}:${password}@blogifyhub.rjugz7a.mongodb.net/?retryWrites=true&w=majority&appName=blogifyHub`);
+        console.log("Connect to MongoDB successfully");
+    }catch (error) {
+        console.log("Connection failed" + error.message);
+    }
+}
+connectDB();   //start DB 
+
+
+app.get("/", async(req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc'});
+  res.render("articles/index", { articles: articles });
 });
 
 app.use("/articles", articleRouter);
 
+//Listing Route
 app.listen(3000, () => {
   console.log(`Server is listening to port 3000`);
 });
